261d2e822ee8d82dbf16d2ef860e5a3b7d4f6d72
controlZ <o_dahyun@gm.gist.ac.kr>
2023-09-18 00:41:11 +0900

    ✨ feat: add idpTokenResponse type

--- src/user/type/user.type.ts
+++ src/user/type/user.type.ts
@@ -1,7 +1,12 @@
 export interface UserInfo {
-  userUuid: string;
-  userEmailId: string;
-  userName: string;
-  userPhoneNumber: string;
-  studentNumber: string;
+  user_uuid: string;
+  user_email_id: string;
+  user_name: string;
+  user_phone_number: string;
+  student_id: string;
 }
+
+export type IdpTokenResponse = {
+  access_token: string;
+  refresh_token: string;
+};
