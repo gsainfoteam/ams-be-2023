9fc7c4d240c7118d0d953f2f251011d7261ba8a6
rabo8497 <67371359+rabo8497@users.noreply.github.com>
2023-09-23 15:18:08 +0900

    delete webpack

--- src/global/config/database/database.service.ts
+++ src/global/config/database/database.service.ts
@@ -11,16 +11,11 @@ export class MySQLConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'mysql',
-      //username: this.configService.get<string>('MYSQL_DATABASE_USER'),
-      //password: this.configService.get<string>('MYSQL_DATABASE_PASSWORD'),
-      //port: this.configService.get<number>('MYSQL_DATABASE_PORT'),
-      //host: this.configService.get<string>('MYSQL_DATABASE_HOST'),
-      //database: this.configService.get<string>('MYSQL_DATABASE_NAME'),
-      username: 'root',
-      password: '1234',
-      port: 3306,
-      host: 'localhost',
-      database: 'ams_test',
+      username: this.configService.get<string>('MYSQL_DATABASE_USER'),
+      password: this.configService.get<string>('MYSQL_DATABASE_PASSWORD'),
+      port: this.configService.get<number>('MYSQL_DATABASE_PORT'),
+      host: this.configService.get<string>('MYSQL_DATABASE_HOST'),
+      database: this.configService.get<string>('MYSQL_DATABASE_NAME'),
       entities: [Project, User],
       synchronize: true,
     };
